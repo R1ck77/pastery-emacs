c353dacb283df8cc806756871826d8affcecb8da
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-08-09 14:09:29 +0200

    Messing around with my mock server
    Without much luck…

--- mock-pastery.el
+++ mock-pastery.el
@@ -7,6 +7,9 @@
     (forward-line)
     (buffer-substring-no-properties (point) (point-max))))
 
+(defun 200-answer (name)
+  (cons 200 (read-sample name)))
+
 (defun stop-mock-server ()
   (interactive)
   (ws-stop-all))
@@ -15,12 +18,11 @@
   (let ((api-key (cdr (assoc "api_key" headers))))
     (cond
      ((null api-key)
-      (read-sample "missing_api_key.txt"))
+      (200-answer "missing_api_key.txt"))
      ((not (equal "mykey" api-key))
-      (read-sample "invalid_api_key.txt")))))
+      (200-answer "invalid_api_key.txt")))))
 
 (defun mock-pastery-response (headers)
-  (message "Headers: %s" headers)
   (or (wrong-api-key headers)
       (let ((delete-value (alist-get ':DELETE headers))
             (get-value (alist-get ':GET headers))
@@ -29,28 +31,40 @@
          (delete-value
           (cond 
            ((equal delete-value "/api/paste/bzgkgz/")
-            (read-sample "delete_paste_ok.txt"))
+            (200-answer "delete_paste_ok.txt"))
            (t
-            (read-sample "delete_paste_fail.txt"))))
+            (200-answer "delete_paste_fail.txt"))))
          (get-value
           (cond 
            ((equal get-value "/api/paste/bzgkgz/")
-            (read-sample "get_paste_ok.txt"))
+            (200-answer "get_paste_ok.txt"))
            ((equal get-value "/api/paste/") ; missing condition for single
-            (read-sample "list_pastes_ok_multi.txt"))
+            (200-answer "list_pastes_ok_multi.txt"))
            (t
-            (read-sample "get_paste_fail.txt"))))
-         (post-value (read-sample "write_paste_ok.txt"))))))
+            (200-answer "get_paste_fail.txt"))))
+         (post-value
+          '(100 . ""))))))
 
-(defun mock-pastery-handler (response)
-  (with-slots (process headers) response
-    (let ((result (mock-pastery-response headers)))
-      (message "Getting the header: %s" headers)
-      (message "Sending %s" result)
-      (ws-response-header process 200
+(defun mock-pastery-handler (request)
+  (message "!!!!!!!!!!!HANDLER CALLED!!!!!!!!")
+  (message "Handler called with: \n>>>>>>\n%s\n<<<<<" request)
+  (with-slots (process headers) request
+    (setq whatever headers)
+    (setq nprocess process)
+    (let ((code-content (mock-pastery-response headers)))
+      (ws-response-header process (car code-content)
                           '("Content Type" . "application/json")
-                          (cons "Content-Length" (number-to-string (length result))))
-      (process-send-string process result))))
+                          (cons "Content-Length" (number-to-string (length (cdr code-content)))))
+      (process-send-string process (cdr code-content))
+      (when (= 100 (car code-content))
+        (ws-web-socket-connect request
+                               (lambda (proc string)
+                                 (message "GOT: %s" string)
+                                 (process-send-string proc
+                                                      (ws-web-socket-frame (concat "you said: " string)))))
+        :keep-alive
+        
+))))
 
 (defun start-mock-server (&optional try-kill)
   (interactive)
