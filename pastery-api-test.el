9ab333b7b1d6e0b913e07f8cb7c4d7b79a01c1df
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-08-12 18:37:08 +0200

    First API function thoroughly tested

--- pastery-api-test.el
+++ pastery-api-test.el
@@ -5,8 +5,19 @@
 (describe "pastery-api"
   (describe "pastery-get-paste-list"
     (it "returns a list of pastes if the api key is correct"
-      
-      )
+      (expect (with-debug-server
+               (let ((pastery-url "localhost:8080"))
+                 (pastery-get-paste-list "mykey")))
+              :to-equal '((pastes . [((id . "bzgkgz")
+                                      (title . "Sample data bis")
+                                      (url . "https://www.pastery.net/bzgkgz/")
+                                      (language . "ttl")
+                                      (duration . 43199))
+                                     ((id . "hmueky")
+                                      (title . "Sample data")
+                                      (url . "https://www.pastery.net/hmueky/")
+                                      (language . "ttl")
+                                      (duration . 43196))]))))
     (it "returns an error if the wrong api key is provided"
       (expect (with-debug-server
                (let ((pastery-url "localhost:8080"))
--- pastery-api.el
+++ pastery-api.el
@@ -6,7 +6,7 @@
 (defun pastery-get-paste-list (api-key)
   (request-response-data
    (request (format "%s/api/paste/" pastery-url)
-            :params '(("api_key" . "wrong_key"))
+            :params `(("api_key" . ,api-key))
             :parser 'json-read
             :sync t)))
 
