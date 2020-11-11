d82563944c5ac338a430f3be592f1c39f03b71aa
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-11-11 13:30:46 +0100

    Some changes to the tests (red)

--- pastery-api-test.el
+++ pastery-api-test.el
@@ -1,30 +1,35 @@
 (require 'buttercup)
 (require 'pastery-api)
-(require 'mock-pastery)
+(require 'ersatz-pastery-debug)
 
 (describe "pastery-api"
   (describe "pastery/get-paste-list"
     (it "returns a list of pastes if the api key is correct"
+
       (expect (with-debug-server
+               (ersatz-debug--set-pastes (list
+                                          (cons "id1" (new-paste :title "title1" :language "ttl" :max-views 12))
+                                          (cons "id2" (new-paste :title "title2" :language "c" :duration 100))))
                (let ((pastery-url "localhost:8080"))
-                 (pastery/get-paste-list "mykey")))
-              :to-equal '((pastes . [((id . "bzgkgz")
-                                      (title . "Sample data bis")
-                                      (url . "https://www.pastery.net/bzgkgz/")
+                 (pastery/get-paste-list "key1")))
+              :to-equal '((pastes . [((id . "id1")
+                                      (title . "title1")
+                                      (url . "https://localhost:8080/id1/")
                                       (language . "ttl")
+                                      (max-views . 12)
                                       (duration . 43199))
-                                     ((id . "hmueky")
-                                      (title . "Sample data")
-                                      (url . "https://www.pastery.net/hmueky/")
-                                      (language . "ttl")
-                                      (duration . 43196))]))))
-    (it "returns an error if the wrong api key is provided"
+                                     ((id . "id2")
+                                      (title . "title2")
+                                      (url . "https://localhost:8080/id2/")
+                                      (language . "c")
+                                      (duration . 100))]))))
+    (xit "returns an error if the wrong api key is provided"
       (expect (with-debug-server
                (let ((pastery-url "localhost:8080"))
                  (pastery/get-paste-list "wrong-api-key")))
               :to-equal '((result . "error")
                           (error_msg . "\"api_key\" must be a valid API key.")))))
-  (describe "pastery/get-paste"
+  (xdescribe "pastery/get-paste"
     (it "returns an error if the API key is wrong"
       (expect (with-debug-server
                (let ((pastery-url "localhost:8080"))
@@ -46,7 +51,7 @@
                           (language . "ttl")
                           (duration . 43196)
                           (body . "def my_function(x):\n    return x + 42\n\nprint(my_function(0))\n")))))
-  (describe "pastery/delete-paste"
+  (xdescribe "pastery/delete-paste"
     (it "returns an error if the API key is wrong"
       (expect (with-debug-server
                (let ((pastery-url "localhost:8080"))
@@ -64,7 +69,7 @@
                (let ((pastery-url "localhost:8080"))
                  (pastery/delete-paste "mykey" "bzgkgz")))
               :to-equal '((result . "success")))))
-  (describe "pastery/put-paste"
+  (xdescribe "pastery/put-paste"
         (it "returns an error if the API key is wrong"
       (expect (with-debug-server
                (let ((pastery-url "localhost:8080"))
